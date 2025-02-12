8043f6e17fbe8c78f5a1d5f63d0c972cf19ae095
Admin <sohilasobhysadek@gmail.com>
2025-02-12 17:02:54 +0200

    Edits2 12/2/2025

--- Index.js
+++ Index.js
@@ -1,29 +1,35 @@
-let sideNavOpen = true;
-document.getElementById("menuBtn").addEventListener('click', function () {
-    const menu = document.getElementById('mainMenu');
-    const rightPart = document.getElementById('rightPart')
-    if (menu.classList.contains("sideMenuClose")) {
-        menu.classList.remove('sideMenuClose');
-        menu.classList.add('sideMenu');
-        rightPart.classList.add('sideNavOpen')
-        rightPart.classList.remove('sideNavClose')
-        document.body.classList.add("Open")
-    } else {
-        menu.classList.add('sideMenuClose');
-        menu.classList.remove('sideMenu');
-        rightPart.classList.remove('sideNavOpen')
-        rightPart.classList.add('sideNavClose')
-        document.body.classList.remove("Open")
-    }
-});
-document.getElementById('closeBtn').addEventListener('click', function () {
-    const menu = document.getElementById('mainMenu');
-    const rightPart = document.getElementById('rightPart')
+const menu = document.getElementById('mainMenu');
+const rightPart = document.getElementById('rightPart')
+function closeMenu() {
     menu.classList.add('sideMenuClose');
     menu.classList.remove('sideMenu');
     rightPart.classList.remove('sideNavOpen')
     rightPart.classList.add('sideNavClose')
     document.body.classList.remove("Open")
+}
+function openMenu() {
+    menu.classList.remove('sideMenuClose');
+    menu.classList.add('sideMenu');
+    rightPart.classList.add('sideNavOpen')
+    rightPart.classList.remove('sideNavClose')
+    document.body.classList.add("Open")
+}
+document.getElementById("menuBtn").addEventListener('click', function () {
+    if (menu.classList.contains("sideMenuClose")) {
+        openMenu()
+    } else {
+        closeMenu()
+    }
+});
+document.getElementById('closeBtn').addEventListener('click', function () {
+    closeMenu()
 });
 
+window.onresize = function () {
+    if (window.innerWidth <= 992) {
+        closeMenu();
+    } else {
+        openMenu()
+    }
+};
 
