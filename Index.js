b311dfbf1f2e5873e0d87343e57b67e1c698be5a
Admin <sohilasobhysadek@gmail.com>
2025-02-12 13:25:00 +0200

    Edits 12/2/2025

--- Index.js
+++ Index.js
@@ -7,11 +7,23 @@ document.getElementById("menuBtn").addEventListener('click', function () {
         menu.classList.add('sideMenu');
         rightPart.classList.add('sideNavOpen')
         rightPart.classList.remove('sideNavClose')
+        document.body.classList.add("Open")
     } else {
         menu.classList.add('sideMenuClose');
         menu.classList.remove('sideMenu');
         rightPart.classList.remove('sideNavOpen')
         rightPart.classList.add('sideNavClose')
+        document.body.classList.remove("Open")
     }
 });
+document.getElementById('closeBtn').addEventListener('click', function () {
+    const menu = document.getElementById('mainMenu');
+    const rightPart = document.getElementById('rightPart')
+    menu.classList.add('sideMenuClose');
+    menu.classList.remove('sideMenu');
+    rightPart.classList.remove('sideNavOpen')
+    rightPart.classList.add('sideNavClose')
+    document.body.classList.remove("Open")
+});
+
 
